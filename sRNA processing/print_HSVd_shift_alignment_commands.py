0fd24501a63a2f05faf56b0766f55dd728bb7c07
diyab3 <diya.bala33@gmail.com>
2026-01-14 13:47:22 -0800

    Add files via upload

---
+++ sRNA processing/print_HSVd_shift_alignment_commands.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+usage = 'Usage: ' + sys.argv[0] + ' <bowtie 2 index name>' + '<fastq files>'
+
+if len(sys.argv) < 3:
+    print(usage)
+    exit()
+
+fastq_files = sys.argv[2:]
+bowtie2_idx = sys.argv[1]
+
+
+for fastq_file in fastq_files:
+    #print(fastq_file)
+    new_sam_suffix = os.path.basename(bowtie2_idx) + ".sam"
+    #print(new_sam_suffix)
+    new_sam_name = fastq_file.replace("unaligned_to_HSVd_reads.fastq", new_sam_suffix)
+    #print(new_sam_name)
+    new_log_suffix = os.path.basename(bowtie2_idx) + ".log"
+    new_log_name = fastq_file.replace("unaligned_to_HSVd_reads.fastq", new_log_suffix)
+    #print(new_log_name)
+
+    command = f'hqsub "bowtie2 -x {bowtie2_idx} -U {fastq_file} -S {new_sam_name}" -q "*" -r {new_log_name}'
+
+    print(command)
+
